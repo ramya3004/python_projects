f5b7519e822022c0428101bdba6fe3a6d86de913
ramya3004 <148035425+ramya3004@users.noreply.github.com>
2024-11-12 20:00:29 +0530

    Add files via upload

---
+++ project python/game.py
@@ -0,0 +1,19 @@
+import random
+user_choice=int(input("Enter your Choice: Type 0 for Rock, 1 for Paper, 2 for Scissors."))
+computer_choice=random.randint(0,2)
+print("Computer Chose:")
+print(computer_choice)
+if computer_choice == user_choice:
+     print("It's a draw.")
+
+elif computer_choice > user_choice: 
+     print("You Lose.")
+
+elif user_choice > computer_choice:
+     print("You win.")
+
+elif computer_choice == 0 and user_choice == 2:
+     print("You lose")
+
+elif user_choice == 0 and computer_choice == 2:
+     print("You win.")
